1b8013a57e6bcd8193cf4a026f7234fe809d112c
vierukundo <vierukundo@gmail.com>
2023-11-30 12:55:48 +0200

    Added Y-axis range

--- Interactive_dashboard/chart_updater.py
+++ Interactive_dashboard/chart_updater.py
@@ -10,7 +10,7 @@ def bar_chart(app, summary_file, other_file):
         )
     
     def chart_updater(chosen_year, chosen_indicator):
-        figure = make_subplots(rows=1, cols=2, specs=[[{'type': 'xy'}, {'type': 'xy'}]], subplot_titles=(f'{chosen_indicator}', "Rwanda Land cover classes"))
+        figure = make_subplots(rows=1, cols=3, specs=[[{'type': 'xy'}, {'type': 'xy'}, {'type': 'xy'}]], subplot_titles=("Season A", "Season B", "Season C"), horizontal_spacing=0.1)
         for sheet in summary_file.sheet_names:
             df = summary_file.parse(sheet)
             if df.columns[0] == chosen_indicator and df.columns[4] == chosen_year:
@@ -18,18 +18,22 @@ def bar_chart(app, summary_file, other_file):
             
         # Create a bar chart with multiple y-values on first column
         figure.add_trace(go.Bar(x=df[df.columns[0]], y=df[df.columns[1]], name=df.columns[1]), row=1, col=1)
-        figure.add_trace(go.Bar(x=df[df.columns[0]], y=df[df.columns[2]], name=df.columns[2]), row=1, col=1)
-        figure.add_trace(go.Bar(x=df[df.columns[0]], y=df[df.columns[3]], name=df.columns[3]), row=1, col=1)
+        figure.add_trace(go.Bar(x=df[df.columns[0]], y=df[df.columns[2]], name=df.columns[2]), row=1, col=2)
+        figure.add_trace(go.Bar(x=df[df.columns[0]], y=df[df.columns[3]], name=df.columns[3]), row=1, col=3)
 
         # create the second chart on second column  
-        df2 = other_file.parse('Sheet1')
-        df2 = df2.sort_values(by=df2.columns[1])
-        figure.add_trace(go.Bar(x=df2[df2.columns[0]], y=df2[df2.columns[1]]), row=1, col=2)
+        # df2 = other_file.parse('Sheet1')
+        # df2 = df2.sort_values(by=df2.columns[1])
+        # figure.add_trace(go.Bar(x=df2[df2.columns[0]], y=df2[df2.columns[1]]), row=1, col=2)
         figure.update_layout(
-            title_text=f'{chosen_year} - {chosen_indicator} in seasons A, B, and C', font=dict(family='Cambria, "Times New Roman", serif', size=16), height=600
+            title_text=f'{chosen_year} - {chosen_indicator} in seasons A, B, and C', font=dict(family='Cambria, "Times New Roman", serif', size=16)
             )
         figure.update_xaxes(title_text=filter.filter_X_axis(chosen_indicator), row=1, col=1)
         figure.update_yaxes(title_text=filter.filter_Y_axis(chosen_indicator), row=1, col=1)
-        figure.update_xaxes(title_text='Land class', row=1, col=2)
-        figure.update_yaxes(title_text='Area in hectares', row=1, col=2)
+        figure.update_xaxes(title_text=filter.filter_X_axis(chosen_indicator), row=1, col=2)
+        figure.update_yaxes(title_text=filter.filter_Y_axis(chosen_indicator), row=1, col=2)
+        figure.update_xaxes(title_text=filter.filter_X_axis(chosen_indicator), row=1, col=3)
+        figure.update_yaxes(title_text=filter.filter_Y_axis(chosen_indicator), row=1, col=3)
+        # figure.update_xaxes(title_text='Land class', row=1, col=2)
+        # figure.update_yaxes(title_text='Area in hectares', row=1, col=2)
         return figure
--- Interactive_dashboard/update_pie.py
+++ Interactive_dashboard/update_pie.py
@@ -17,7 +17,7 @@ def pie_chart_updater(app, other_file):
         if year == 2021:
             new_sheets = other_file.sheet_names[1:13]
         else:
-            new_sheets = other_file.sheet_names[1:]
+            new_sheets = other_file.sheet_names
         for sh_name in new_sheets:
             df = other_file.parse(sh_name)
             if df.columns[0] not in names:
@@ -30,21 +30,21 @@ def pie_chart_updater(app, other_file):
         [Input('years-dropdown', 'value'), Input('names', 'value')]
         )
     def update_data(year, name):
-        for sheet in other_file.sheet_names[1:]:
+        for sheet in other_file.sheet_names:
             pie_df = other_file.parse(sheet)
             if pie_df.columns[0] == name and year == pie_df.columns[-1]:
                 break
-        return pie_df.columns[1:-2], pie_df.columns[1]
+        return pie_df.columns[:-2], pie_df.columns[1]
     # This updates the piecharts according to year and dataset selected
     @app.callback(
         Output('pie-charts-1', 'figure'),
         [Input('years-dropdown', 'value'), Input('names', 'value'), Input('data', 'value')]
         )
     def pie_updater_one(year, name, data):
-        fig_pie = make_subplots(rows=1, cols=3, specs=[[{'type':'domain'}, {'type':'domain'}, {'type':'domain'}]], subplot_titles=("Season A", "Season B", "Season C"))
-        fig_line = make_subplots(rows=1, cols=3, specs=[[{'type':'xy'}, {'type':'xy'}, {'type':'xy'}]], subplot_titles=("Season A", "Season B", "Season C"))
+        fig_pie = make_subplots(rows=1, cols=3, specs=[[{'type':'domain'}, {'type':'domain'}, {'type':'domain'}]], subplot_titles=("Season A", "Season B", "Season C"), horizontal_spacing=0.1)
+        fig_line = make_subplots(rows=1, cols=3, specs=[[{'type':'xy'}, {'type':'xy'}, {'type':'xy'}]], subplot_titles=("Season A", "Season B", "Season C"), horizontal_spacing=0.1)
         
-        for sheet in other_file.sheet_names[1:]:
+        for sheet in other_file.sheet_names:
             pie_df = other_file.parse(sheet)
             if pie_df.columns[0] == name and year in pie_df.columns:
                 season = None
@@ -59,14 +59,19 @@ def pie_chart_updater(app, other_file):
                     fig_pie.add_trace(go.Pie(labels=pie_df[pie_df.columns[0]].tolist(), values=pie_df[data].tolist()), 1, 1 + seasons.index(season))
                 elif season and len(pie_df.columns) >= 7:
                     fig_line.add_trace(go.Scatter(x=pie_df[pie_df.columns[0]], y=pie_df[data], mode='lines+markers', name='markers'), 1, 1 + seasons.index(season))
+                else:
+                    fig_line = make_subplots(rows=1, cols=2, specs=[[{'type':'xy'}, {'type':'xy'}]], subplot_titles=("Area", "Percentage (%)"), horizontal_spacing=0.1)
+                    fig_line.add_trace(go.Scatter(x=pie_df[pie_df.columns[0]], y=pie_df[pie_df.columns[1]], mode='lines+markers', name='markers'), 1, 1)
+                    fig_line.add_trace(go.Scatter(x=pie_df[pie_df.columns[0]], y=pie_df[pie_df.columns[2]], mode='lines+markers', name='markers'), 1, 2)
+                    break
             if len(fig_pie['data']) == 3 or len(fig_line['data']) == 3:
                 break
         if len(fig_pie['data']) == 3:
             fig_pie.update_traces(textposition='inside')
-        else:
+        elif len(fig_line['data']) == 3: 
             for i in range(3):
                 fig_line.update_xaxes(title_text=filter_X_axis(name), row=1, col= i + 1)
-                fig_line.update_yaxes(title_text="Percentage (%)", row=1, col= i + 1)
+                fig_line.update_yaxes(title_text="Percentage (%)", row=1, col= i + 1, range=[0, 100])
         fig = fig_pie if len(fig_pie['data']) == 3 else fig_line
         fig.update_layout(title_text=name, font=dict(family='Cambria, "Times New Roman", serif', size=16), height=500)
 
